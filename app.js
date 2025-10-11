cd4284960620643f0f06a27dd728393c0b9bb00c
Anton <ibeliev@gmail.com>
2025-10-11 16:04:55 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.send('hi');
+});
+
+app.listen(5000);
