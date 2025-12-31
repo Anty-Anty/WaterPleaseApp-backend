355a95cf7edd2d225f2f9567c65bb865aad54091
Anton <ibeliev@gmail.com>
2025-12-31 08:24:00 -0800

    Add MongoDB Atlas connection configuration and enviromental variables

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+require("dotenv").config();
 
 const usersRoutes = require('./routes/users-routes');
 const mapsRoutes = require('./routes/maps-routes');
 const plantsRoutes = require('./routes/plants-routes');
-const HttpError = require ('./models/http-error');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
@@ -16,21 +18,40 @@ app.use(bodyParser.json());
 app.use('/api/maps', mapsRoutes);
 app.use('/api/plants', plantsRoutes);
 
-app.use((req,res,next)=>{
-    return next(new HttpError('Could not find this route.', 404));
+app.use((req, res, next) => {
+  return next(new HttpError('Could not find this route.', 404));
 });
 
 //error handling middleware, this func will execute if any middleware infront gives an error
-app.use((error, req, res, next)=>{
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occurred!'});
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 // app.use((req, res, next) => {
 //   res.send('hi');
 // });
 
-app.listen(5000);
+// app.listen(5000);
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9dw6mwc.mongodb.net/?appName=Cluster0`,
+  {
+    dbName: process.env.DB_NAME
+  }
+) 
+
+  .then(() => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  //confirm that connection to database works:
+//   mongoose.connection.once("open", () => {
+//   console.log("Connected to DB:", mongoose.connection.name);
+// });
